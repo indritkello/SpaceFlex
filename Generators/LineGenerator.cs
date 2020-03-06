7e264ff65f1f19075059d58f3026c62e99c1bd29
= <=>
2020-03-07 00:29:52 +0100

    Worked on PongGame. Added helper class for generation left right "players". Built seperate worlds and effect. Left is controled by W and S, while Right be up and down keys.

---
+++ Generators/LineGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceFlex.Generators
+{
+    public class LineGenerator
+    {
+        public static VertexPositionColor[]  CreateRectangleLine(int topLeftX, int topLeftY, int width, int height, Color solidColor)
+        {
+            var verts = new VertexPositionColor[6];
+            //First triangle part
+            verts[0] = new VertexPositionColor(new Vector3(topLeftX, topLeftY, 0), solidColor);
+            verts[1] = new VertexPositionColor(new Vector3(topLeftX + width, topLeftY, 0), solidColor);
+            verts[2] = new VertexPositionColor(new Vector3(topLeftX + width, topLeftY - height, 0), solidColor);
+
+            //Second part
+            verts[3] = new VertexPositionColor(new Vector3(topLeftX + width, topLeftY - height, 0), solidColor);
+            verts[4] = new VertexPositionColor(new Vector3(topLeftX, topLeftY - height, 0), solidColor);
+            verts[5] = new VertexPositionColor(new Vector3(topLeftX, topLeftY, 0), solidColor);
+            return verts;
+        }
+    }
+}
--- PongGame.cs
+++ PongGame.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SpaceFlex.GameElements;
+using SpaceFlex.Generators;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SpaceFlex
 {
@@ -12,10 +15,14 @@ namespace SpaceFlex
 
         /*  Game elements   */
         Camera camera;
-        VertexPositionColor[] verts;
+        List<VertexPositionColor> allVertices = new List<VertexPositionColor>();
+        VertexPositionColor[] vertsRight;
+        VertexPositionColor[] vertsLeft;
         VertexBuffer vertexBuffer;
-        BasicEffect effect;
-        Matrix world = Matrix.Identity;
+        BasicEffect rEffect;
+        BasicEffect lEffect;
+        Matrix rWorld = Matrix.Identity;
+        Matrix lWorld = Matrix.Identity;
         public PongGame()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -26,7 +33,7 @@ namespace SpaceFlex
         protected override void Initialize()
         {
             // Initialize camera 
-            camera = new Camera(this, new Vector3(0, 0, 5),
+            camera = new Camera(this, new Vector3(0, 0, 10),
                                 Vector3.Zero,
                                 Vector3.Up);
             Components.Add(camera);
@@ -39,20 +46,18 @@ namespace SpaceFlex
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Initialize vertices 
-            verts = new VertexPositionColor[6];
-            verts[0] = new VertexPositionColor(new Vector3(3, 3, 0), Color.Green);
-            verts[1] = new VertexPositionColor(new Vector3(3, -3, 0), Color.Green);
-            verts[2] = new VertexPositionColor(new Vector3(4, -3, 0), Color.Green);
-            verts[3] = new VertexPositionColor(new Vector3(4, -3, 0), Color.Green);
-            verts[4] = new VertexPositionColor(new Vector3(3, 3, 0), Color.Green);
-            verts[5] = new VertexPositionColor(new Vector3(4, 3, 0), Color.Green);
-
+            vertsRight = LineGenerator.CreateRectangleLine(9, 3, 1, 6, Color.DarkBlue);
+            vertsLeft = LineGenerator.CreateRectangleLine(-9, 3, 1, 6, Color.DarkRed);
+            
+            allVertices.AddRange(vertsLeft);
+            allVertices.AddRange(vertsRight);
             // Set vertex data in VertexBuffer 
             vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor),
-                                            verts.Length, BufferUsage.None);
-            vertexBuffer.SetData(verts);
+                                            allVertices.Count, BufferUsage.None);
+            vertexBuffer.SetData(allVertices.ToArray());
             // Initialize the BasicEffect 
-            effect = new BasicEffect(GraphicsDevice);
+            rEffect = new BasicEffect(GraphicsDevice);
+            lEffect = new BasicEffect(GraphicsDevice);
 
             // Set cullmode to none (Usually should not be set to none, only for debugging)
             RasterizerState rs = new RasterizerState();
@@ -65,17 +70,26 @@ namespace SpaceFlex
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // Translation
-            KeyboardState keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Down)){
-                if(world.Translation.Y>(-1))
-                world *= Matrix.CreateTranslation(0, -.01f, 0);
+            // Translation Right
+            KeyboardState rightKeyboardState = Keyboard.GetState();
+            if (rightKeyboardState.IsKeyDown(Keys.Down)){
+                if(rWorld.Translation.Y>(-1))
+                    rWorld *= Matrix.CreateTranslation(0, -.01f, 0);
             }
-            if (keyboardState.IsKeyDown(Keys.Up))
-                world *= Matrix.CreateTranslation(0, .01f, 0);
+            if (rightKeyboardState.IsKeyDown(Keys.Up))
+                rWorld *= Matrix.CreateTranslation(0, .01f, 0);
 
 
-            //Note! world *= is done to keep the translation that is done before, if you use just world=, you will lose it
+            // Translation Right
+            KeyboardState leftKeyboardState = Keyboard.GetState();
+            if (leftKeyboardState.IsKeyDown(Keys.S))
+            {
+                if (lWorld.Translation.Y > (-1))
+                    lWorld *= Matrix.CreateTranslation(0, -.01f, 0);
+            }
+            if (leftKeyboardState.IsKeyDown(Keys.W))
+                lWorld *= Matrix.CreateTranslation(0, .01f, 0);
+
             base.Update(gameTime);
         }
 
@@ -87,16 +101,29 @@ namespace SpaceFlex
             GraphicsDevice.SetVertexBuffer(vertexBuffer);
 
             //Set object and camera info 
-            effect.World = Matrix.CreateScale(.5f) * world;
-            effect.View = camera.view;
-            effect.Projection = camera.projection;
-            effect.VertexColorEnabled = true;
+            rEffect.World = Matrix.CreateScale(.5f) * rWorld;
+            rEffect.View = camera.view;
+            rEffect.Projection = camera.projection;
+            rEffect.VertexColorEnabled = true;
             // Begin effect and draw for each pass 
-            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            foreach (EffectPass pass in rEffect.CurrentTechnique.Passes)
             {
                 pass.Apply();
-                GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, verts, 0, 2);
+                GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, vertsRight, 0, 2);
             }
+
+            //Set object and camera info 
+            lEffect.World = Matrix.CreateScale(.5f) * lWorld;
+            lEffect.View = camera.view;
+            lEffect.Projection = camera.projection;
+            lEffect.VertexColorEnabled = true;
+            // Begin effect and draw for each pass 
+            foreach (EffectPass pass in lEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleList, vertsLeft, 0, 2);
+            }
+
             base.Draw(gameTime);
         }
     }
